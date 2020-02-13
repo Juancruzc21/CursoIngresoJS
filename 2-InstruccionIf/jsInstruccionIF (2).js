08b43227838a6565a7538929aa5d6dd3ea913598
Juancruzc21 <60520612+Juancruzc21@users.noreply.github.com>
2020-02-12 23:41:53 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -2,6 +2,11 @@ function mostrar()
 {
 //tomo la edad  
 
+	var edad;
 
+	edad = document.getElementById('edad').value;
+	edad = parseInt(edad);
+	
+	if (edad >= 18) alert("Bien capo");
 
 }//FIN DE LA FUNCIÓN
